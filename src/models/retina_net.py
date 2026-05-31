5fcd5236d521ba4a17b33d66c841c9664efd6090
infernoWolf99 <kyeiseth58@gmail.com>
2026-05-31 12:20:49 +0000

    added retinanet model

---
+++ src/models/retina_net.py
@@ -0,0 +1,28 @@
+import torchvision
+from torchvision.models.detection.retinanet import RetinaNet_ResNet50_FPN_Weights
+
+
+class RetinaNet:
+    """
+    A wrapper class for initializing RetinaNet models for malaria detection.
+    """
+
+    def __init__(self, num_classes: int, pre_trained: bool = False):
+        self.num_classes = num_classes
+        self.pre_trained = pre_trained
+        self.model = self._build_model()
+
+    def _build_model(self):
+        if self.pre_trained:
+            return RetinaNet_ResNet50_FPN_Weights.DEFAULT
+        else:
+            return None
+
+        model = torchvision.models.detection.retinanet_resnet50_fpn(
+            weights=weights, num_classes=self.num_classes
+        )
+        return model
+
+    def get_model(self):
+        """Returns the raw PyTorch model execution graph."""
+        return self.model
