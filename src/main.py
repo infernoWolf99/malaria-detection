9c12b3ae7fbc86bc1f8688ef9dc1166647c9dfcc
infernoWolf99 <kyeiseth58@gmail.com>
2026-05-31 17:49:57 +0000

    add main script and configuration file for malaria detection pipeline

---
+++ src/main.py
@@ -0,0 +1,45 @@
+import argparse
+import os
+from pathlib import Path
+
+import yaml
+
+
+def get_data_dir(path: str = '../data') -> Path:
+    """
+    Auto-detects if running on Compute Canada via SLURM environment variables.
+    Falls back to local path if running on a local machine.
+    """
+
+    slurm_tmp = os.environ.get("SLURM_TMPDIR")
+    if slurm_tmp:
+        print(f"Using Compute Canada. Reading data from: {slurm_tmp}")
+        return Path(slurm_tmp) / "data"
+    else:
+        print(f"Running on local machine detected. Reading data from: {path}")
+        return Path(path)
+
+def main():
+    parser = argparse.ArgumentParser(description="Malaria Detection Training Pipeline")
+    parser.add_argument(
+        "--config", 
+        type=str, 
+        required=True, 
+        help="Path to the model yaml config file (e.g., config/model_resnet50.yaml)"
+    )
+    args = parser.parse_args()
+
+    # Load model configurations
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+    
+    print(f"Loaded configuration: {config['experiment_name']}")
+
+    #  data paths
+    data_dir = get_data_dir(path=config["data"]["path"])
+
+
+    print("Beginning execution pipeline setup...")
+
+if __name__ == "__main__":
+    main()
